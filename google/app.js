ae5f9f28191636abad2aceb205b31115b7e765f7
nikub444 <magaszak12@gmail.com>
2021-04-13 17:26:15 +0200

    searchresults added

--- google/app.js
+++ google/app.js
@@ -1,10 +1,13 @@
+
 var app = new Vue({ 
     el: '#app',
     data: {
-        inputHandler: ''
+        inputHandler: '',
+        cities: window.cities
     },
     updated: function() {
         if(app.inputHandler !== '')
             document.body.classList.add("results")
+            console.log(app.cities)
       }
 });
